e779688407cb258e86a41da123ae4f8269ec9fe7
IvanKahinga <ikahinga@gmail.com>
2023-08-10 16:58:35 +0300

    Checked warnings for deploy display

--- src/components/ProjectSlider.js
+++ src/components/ProjectSlider.js
@@ -1,41 +1,40 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { projectsData } from '../data';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { projectsData } from "../data";
 
-import 'swiper/swiper-bundle.min.css'; // Import Swiper styles
-import SwiperCore, { Navigation } from 'swiper'; // Import Swiper modules
-SwiperCore.use([Navigation]); // Initialize Navigation module
+import "swiper/css";
+import "swiper/css/pagination";
+
+import SwiperCore, { Pagination } from "swiper";
+SwiperCore.use([Pagination]);
 
 function ProjectSlider() {
-
-
   return (
     <div className="flex justify-center">
       <Swiper
         grabCursor={true}
-        navigation={true} // Enable navigation arrows
+        pagination={{ clickable: true }} // Enable pagination
         breakpoints={{
-          320: { slidesPerView: 1, spaceBetween: 20 }, // Adjust spaceBetween as needed
-          // Add more breakpoints if necessary
+          320: {
+            slidesPerView: 1, // Display all slides
+            centeredSlides: true, // Centered slides
+            spaceBetween: 20,
+          },
         }}
       >
         {projectsData.map((project, index) => (
           <SwiperSlide key={index}>
-            <div className="w-full">
+            <div className="">
               <div
                 className="relative flex flex-col items-center justify-center shadow-md rounded-md p-4"
                 style={{
                   backgroundImage: `url('${project.image}')`,
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center',
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
                 }}
               >
                 <div className="w-40 h-40 relative z-10">
-                  <img
-                    src={project.image}
-                    alt=""
-
-                  />
+                  <img src={project.image} alt="" />
                 </div>
                 <div className="mt-4 relative z-10">
                   <h2 className="text-lg font-semibold text-black">
--- src/components/ServicesSlider.js
+++ src/components/ServicesSlider.js
@@ -1,33 +1,46 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
-//import swiper styles
+// Import swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 
-//import data
+// Import Swiper modules
+import SwiperCore, { Pagination } from "swiper";
+
+// Import data
 import { ourServices } from "../data";
+
+// Initialize Swiper modules
+SwiperCore.use([Pagination]);
 
 function ServicesSlider() {
   return (
-    <Swiper grabCursor= {true} breakpoints= {{
+    <Swiper
+      grabCursor={true}
+      pagination={{ clickable: true }} // Enable pagination
+      breakpoints={{
         320: {
-            slidesPerView: 1,
-            spaceBetween:40,
-        }
-    }}>
+          slidesPerView: "auto", // Display all slides
+          centeredSlides: true, // Centered slides
+          spaceBetween: 20,
+        },
+      }}
+    >
       {ourServices.services.map((service, index) => {
-        return <SwiperSlide className="max-w-[256px]" key={index}>
-        <Link to={`/service/${service.name.toLowerCase()}`}>
-            <div className="relative transition duration-300 hover:scale-75">
-              <img src={service.image.type} alt="" />
-              <div className="absolute text-white bottom-[20px] w-full text-center text-[18px] lg:text-2xl font-medium capitalize ">
-                {service.name}
+        return (
+          <SwiperSlide className="max-w-[256px]" key={index}>
+            <Link to={`/service/${service.name.toLowerCase()}`}>
+              <div className="relative transition duration-300 hover:scale-75">
+                <img src={service.image.type} alt="" />
+                <div className="absolute text-white bottom-[20px] w-full text-center text-[18px] lg:text-2xl font-medium capitalize ">
+                  {service.name}
+                </div>
               </div>
-            </div>
-          </Link>
-        </SwiperSlide>;
+            </Link>
+          </SwiperSlide>
+        );
       })}
     </Swiper>
   );
